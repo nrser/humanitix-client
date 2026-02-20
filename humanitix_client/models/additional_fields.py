eb03b435b17aa8689552a08fea5f3711e179ddc0
nrser <neil@neilsouza.com>
2026-02-19 20:46:28 -0800

    Regenerate as v1.20.0.1

--- humanitix_client/models/additional_fields.py
+++ humanitix_client/models/additional_fields.py
@@ -20,12 +20,12 @@ class AdditionalFields:
     """
     Attributes:
         question_id (str):  Example: 5ac5c5e85aec29000ff064f4.
-        value (str):  Example: Gluten Free.
+        value (str | Unset):  Example: Gluten Free.
         details (AdditionalFieldsDetails | Unset):
     """
 
     question_id: str
-    value: str
+    value: str | Unset = UNSET
     details: AdditionalFieldsDetails | Unset = UNSET
     additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
 
@@ -43,9 +43,10 @@ class AdditionalFields:
         field_dict.update(
             {
                 "questionId": question_id,
-                "value": value,
             }
         )
+        if value is not UNSET:
+            field_dict["value"] = value
         if details is not UNSET:
             field_dict["details"] = details
 
@@ -58,7 +59,7 @@ class AdditionalFields:
         d = dict(src_dict)
         question_id = d.pop("questionId")
 
-        value = d.pop("value")
+        value = d.pop("value", UNSET)
 
         _details = d.pop("details", UNSET)
         details: AdditionalFieldsDetails | Unset
--- humanitix_client/models/sales_channel.py
+++ humanitix_client/models/sales_channel.py
@@ -2,8 +2,10 @@ from enum import Enum
 
 
 class SalesChannel(str, Enum):
+    BOXOFFICE = "boxOffice"
     MANUAL = "manual"
     ONLINE = "online"
+    SWAP = "swap"
 
     def __str__(self) -> str:
         return str(self.value)
